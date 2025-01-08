6581427c6e80d817f851fd8a90fac7cf68175113
newgenai <newgenai79@gmail.com>
2025-01-08 19:57:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import sys
 import os
+import gradio as gr
+import torch
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 scripts_dir = os.path.join(current_dir, 'scripts')
 sys.path.append(scripts_dir)
 tabs_dir = os.path.join(current_dir, 'tabs')
 sys.path.append(tabs_dir)
-import gradio as gr
-import torch
-
 
 from tab1 import create_multi_model_tab
 
+multi_model_pipe = None
 
 with gr.Blocks() as demo:
     gr.Markdown("# Text 2 Image Generation")
